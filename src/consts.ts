8a468f15e94d7c24972989f62eae9eba771a1c1b
Nathan <ngage@smu.edu>
2023-10-13 00:57:19 -0500

    add: arimo font

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'nathan___gage';
+export const SITE_DESCRIPTION = 'NATHAN GAGE\'S PERSONAL SITE';
