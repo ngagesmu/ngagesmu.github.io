bfa472d0b15a0b6a3a6fda59d64f62056d4bd6e4
Nathan <ngage@smu.edu>
2023-10-13 13:24:38 -0500

    add: tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,11 @@ export default {
 			fontFamily: {
 				sans: ['Arimo', ...defaultTheme.fontFamily.sans],
 			},
+			colors: {
+				blue: "#64b5f6",
+				white: "#ffffff",
+				black: "#121212",
+			},
 		},
 	},
 	plugins: [],
