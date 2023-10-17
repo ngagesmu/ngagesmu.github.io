01030abd2af38d5bf0f10c047d88d2810dee8a58
Nathan <ngage@smu.edu>
2023-10-16 22:27:56 -0500

    add: posts index

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,29 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: z.string().optional(),
+    }),
 });
 
-export const collections = { blog };
+const post = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        short_title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+
+        // expecting to keep this largely unused
+        heroImage: z.string().optional(),
+    })
+});
+
+export const collections = { blog, post };
