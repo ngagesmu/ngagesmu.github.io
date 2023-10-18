2f5a3d18d231d5ab686a00b28dd6703179026873
Nathan Gage <nathan.gage@cognitivespace.com>
2023-10-17 19:54:23 -0500

    add: full styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nathangage.co",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus",
+      wrap: true,
+    },
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-	mode: 'jit',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Arimo', ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				blue: "#64b5f6",
-				white: "#ffffff",
-				black: "#121212",
-			},
-		},
-	},
-	plugins: [],
-}
+  mode: "jit",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    listStyleType: {
+      none: "none",
+      circle: "circle",
+      decimal: "decimal",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
+        title: ["Arimo", ...defaultTheme.fontFamily.sans],
+        mono: ["Comic Mono", ...defaultTheme.fontFamily.mono],
+        serif: ["Radley", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        blue: "#64b5f6",
+        white: "#ffffff",
+        black: "#121212",
+      },
+    },
+  },
+  plugins: [],
+};
